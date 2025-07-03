141645fb4a723f083560441940e51b14551f5054
Abdul Basit <abdulbasitbasit076@gmail.com>
2025-07-03 17:21:12 +0500

    file structure established

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv"
+
+const app = express();
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+    console.log(`APP listening on port ${port}`);
+});
