774171bcd185c248f5445f59034a60f92ef4abfd
Abdul Basit <abdulbasitbasit076@gmail.com>
2025-07-04 17:27:13 +0500

    react install and backend registeration done

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import connectDB from "./connection/DB.js";
+import routes from "./routes/AuthRoute.js";
 
 const app = express();
 
 dotenv.config();
+connectDB();
 
 app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT;
 
+app.use("/api/v1/auth", routes);
+
 app.listen(port, () => {
-    console.log(`APP listening on port ${port}`);
+  console.log(`APP listening on port ${port}`);
 });
---
+++ server/models/AuthSchema.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    role:{
+        type: String,
+        enum: ['farmer', 'buyer'],
+        required: true
+    },
+    isAdmin: {
+        type: Number,
+        default: 0
+    }
+},{
+    timestamps: true
+});
+
+const user = mongoose.model('FarmSol', userSchema);
+export default user;
