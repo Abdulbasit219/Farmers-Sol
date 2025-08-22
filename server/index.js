c57e2539547dbdda64a57209423b93434e5a87a2
Abdul Basit <abdulbasitbasit076@gmail.com>
2025-08-22 23:36:20 +0500

    try to fix cors error 2

--- server/index.js
+++ server/index.js
@@ -15,8 +15,8 @@ connectDB();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true,
+    origin: "http://localhost:5173", // Frontend URL
+    credentials: true, // Allow cookies
   })
 );
 
