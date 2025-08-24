53eabceaaa0d4728568e00ee75ec5dc860d74db8
Abdul Basit <abdulbasitbasit076@gmail.com>
2025-08-25 00:31:49 +0500

    trying to resolve deployemnt cookie issues

--- client/src/redux/ApiSlice.js
+++ client/src/redux/ApiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const ApiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8080",
+    baseUrl: import.meta.env.MODE === "development" ? "http://localhost:8080" : "/",
     credentials: "include",
   }),
   endpoints: (builder) => ({
